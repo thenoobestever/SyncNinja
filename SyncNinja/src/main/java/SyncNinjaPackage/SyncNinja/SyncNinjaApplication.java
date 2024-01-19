90aaeb95e87df6f2f434f7a31e5f186ed4937cf6
NadiaAlshariah <nadiakalshariah@gmail.com>
2024-01-19 19:52:18 +0300

    spring.run added

--- SyncNinja/src/main/java/SyncNinjaPackage/SyncNinja/SyncNinjaApplication.java
+++ SyncNinja/src/main/java/SyncNinjaPackage/SyncNinja/SyncNinjaApplication.java
@@ -7,6 +7,7 @@ import picocli.CommandLine;
 @SpringBootApplication
 public class SyncNinjaApplication {
 	public static void main(String[] args) {
+		SpringApplication.run(SyncNinjaApplication.class,args);
 		new CommandLine(new HelloCommand()).execute(args);
 	}
 }
