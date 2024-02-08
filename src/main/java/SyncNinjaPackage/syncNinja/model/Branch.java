5228cabfd6111e29289bfbf425d05ccdbde789db
NadiaAlshariah <nadiakalshariah@gmail.com>
2024-02-08 17:46:44 +0300

    sync nodes created

--- src/main/java/SyncNinjaPackage/syncNinja/model/Branch.java
+++ src/main/java/SyncNinjaPackage/syncNinja/model/Branch.java
@@ -2,33 +2,17 @@ package SyncNinjaPackage.syncNinja.model;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
-
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 @Node
-public class Branch {
-    @Id
-    private String id;
+public class Branch extends NinjaNode {
     private String name;
 
-
-
-
-    public Branch() {
-    }
-
     public Branch(String name) {
-        this.id = RandomStringUtils.randomAlphanumeric(16);;
+        super();
         this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getName() {
