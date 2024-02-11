88d2e3ba1f1dc3c98b1edea4fd9f7abc242d151a
NadiaAlshariah <nadiakalshariah@gmail.com>
2024-02-11 14:41:38 +0300

    delete unused imports

--- src/main/java/SyncNinjaPackage/syncNinja/command/StatusCommand.java
+++ src/main/java/SyncNinjaPackage/syncNinja/command/StatusCommand.java
@@ -1,7 +1,5 @@
 package SyncNinjaPackage.syncNinja.command;
 
-import SyncNinjaPackage.syncNinja.model.Directory;
-import SyncNinjaPackage.syncNinja.service.DirectoryService;
 import SyncNinjaPackage.syncNinja.service.StatusService;
 import SyncNinjaPackage.syncNinja.util.SpringAdapter;
 import picocli.CommandLine;
@@ -10,12 +8,10 @@ import picocli.CommandLine;
 public class StatusCommand implements Runnable{
 
     StatusService statusService = SpringAdapter.getBean(StatusService.class);
-    DirectoryService directoryService = SpringAdapter.getBean(DirectoryService.class);
     @Override
     public void run() {
         String path = System.getProperty("user.dir");
         try {
-            Directory directory= directoryService.getDirectory(path);
             statusService.getStatus(path);
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- src/main/java/SyncNinjaPackage/syncNinja/repository/DirectoryRepository.java
+++ src/main/java/SyncNinjaPackage/syncNinja/repository/DirectoryRepository.java
@@ -1,13 +1,7 @@
 package SyncNinjaPackage.syncNinja.repository;
 
-import SyncNinjaPackage.syncNinja.model.Branch;
 import SyncNinjaPackage.syncNinja.model.Directory;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.data.neo4j.repository.query.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 
 public interface DirectoryRepository extends Neo4jRepository<Directory, String> {
 
