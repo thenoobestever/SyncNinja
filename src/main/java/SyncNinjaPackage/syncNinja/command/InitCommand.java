028e30308de7454a413e8a146a55af397303a479
LeenJaber1 <jaberleen95@gmail.com>
2024-02-12 23:52:16 +0300

    updated the status service and the init command

--- src/main/java/SyncNinjaPackage/syncNinja/command/InitCommand.java
+++ src/main/java/SyncNinjaPackage/syncNinja/command/InitCommand.java
@@ -1,5 +1,6 @@
 package SyncNinjaPackage.syncNinja.command;
 
+import SyncNinjaPackage.syncNinja.StateTree.StateTreeServices.StateTreeService;
 import SyncNinjaPackage.syncNinja.model.Directory;
 import SyncNinjaPackage.syncNinja.service.DirectoryService;
 import SyncNinjaPackage.syncNinja.util.SpringAdapter;
@@ -8,13 +9,14 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "init")
 public class InitCommand implements Runnable {
     DirectoryService directoryService = SpringAdapter.getBean(DirectoryService.class);
-
+    StateTreeService stateTreeService = SpringAdapter.getBean(StateTreeService.class);
     @Override
     public void run() {
         String path = System.getProperty("user.dir");
         try {
             Directory directory = directoryService.createDirectory(path);
             directoryService.createDirectoryMainBranch(directory, "main");
+            stateTreeService.generateStateDirectoryNode(path);
 
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
