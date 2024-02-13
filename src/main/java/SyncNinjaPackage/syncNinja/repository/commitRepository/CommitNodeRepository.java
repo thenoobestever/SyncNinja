5a0a9a6f35873639e308d4332e5bf17f7387db9c
Mohammed Alzawawi <thenoobestever@gmail.com>
2024-02-13 19:06:05 +0300

    Revert "commitTreeService"
    This reverts commit bb841dba4f2199df1c9c12ae0f0ac6e79b1cfd65.

--- src/main/java/SyncNinjaPackage/syncNinja/repository/commitRepository/CommitNodeRepository.java
+++
@@ -1,7 +0,0 @@
-package SyncNinjaPackage.syncNinja.repository.commitRepository;
-
-import SyncNinjaPackage.syncNinja.model.commitTree.CommitNode;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface CommitNodeRepository extends Neo4jRepository<CommitNode,String> {
-}
